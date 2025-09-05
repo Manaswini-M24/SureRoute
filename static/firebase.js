2afe2844d192aff39e17be14ac729c9a27da1ad7
Hrithika S <hrithikaskulal@gmail.com>
2025-09-05 23:09:48 +0530

    Add firebase.js for driver portal

---
+++ static/firebase.js
@@ -0,0 +1,18 @@
+
+ 
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCwhbI3PxOT6xN-KejyiuC2GrpBhkmEC8o",
+    authDomain: "driver-login-portal.firebaseapp.com",
+    projectId: "driver-login-portal",
+    storageBucket: "driver-login-portal.firebasestorage.app",
+    messagingSenderId: "54907190318",
+    appId: "1:54907190318:web:5c063f644c1e6fa876b3bf"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
